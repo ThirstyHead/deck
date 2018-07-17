7e42cd29053de8c64b80ad276716294d47273303
Scott Davis <scott@thirstyhead.com>
2018-07-17 17:24:11 -0600

    added Card.SUITES

--- card-test.mjs
+++ card-test.mjs
@@ -6,12 +6,46 @@ export {tests, test};
 let tests = new Map();
 
 tests.set("it should have 4 suites", () => {
-  return Card.SUITES.includes(Card.SPADES) &&
-         Card.SUITES.includes(Card.HEARTS) &&
-         Card.SUITES.includes(Card.DIAMONDS) &&
-         Card.SUITES.includes(Card.CLUBS) &&
-         Card.SUITES.length === 4;
-})
+  return Object.keys(Card.SUITES).length === 4 &&
+         Card.SUITES.hasOwnProperty('HEARTS') &&
+         Card.SUITES.hasOwnProperty('SPADES') &&
+         Card.SUITES.hasOwnProperty('CLUBS') &&
+         Card.SUITES.hasOwnProperty('DIAMONDS');
+});
+
+tests.set("you cannot change the suites", () => {
+  try{
+    Card.SUITES = 'foo';
+    return false; // due to expected error,
+                  // this line should never be reached
+  }catch(error){
+    // console.log(error);
+    return error.name === 'TypeError';
+  }
+});
+
+tests.set("you cannot add a new suite", () => {
+  try{
+    Card.SUITES.FOO = {name:'foo'};
+    return false; // due to expected error,
+                  // this line should never be reached
+  }catch(error){
+    // console.log(error);
+    return error.name === 'TypeError';
+  }
+});
+
+tests.set("you cannot change an existing suite", () => {
+  try{
+    Card.SUITES.HEARTS = {name:'foo'};
+    return false; // due to expected error,
+                  // this line should never be reached
+  }catch(error){
+    // console.log(error);
+    return error.name === 'TypeError';
+  }
+});
+
 
 function test(){
   let results = [];
--- card.mjs
+++ card.mjs
@@ -1,33 +1,13 @@
 'use strict';
 
-const SPADES = 'spades';
-const HEARTS = 'hearts';
-const DIAMONDS = 'diamonds';
-const CLUBS = 'clubs';
-const SUITES = [SPADES, HEARTS, DIAMONDS, CLUBS];
-
+const SUITES = Object.freeze({
+  SPADES   : {name:'spades'},
+  HEARTS   : {name:'hearts'},
+  DIAMONDS : {name:'diamonds'},
+  CLUBS    : {name:'clubs'}
+});
 
 export class Card{
-  constructor(suite=Card.SPADES){
-    this.suite = suite;
-  }
-
-  static get SPADES(){
-    return SPADES;
-  }
-
-  static get HEARTS(){
-    return HEARTS;
-  }
-
-  static get DIAMONDS(){
-    return DIAMONDS;
-  }
-
-  static get CLUBS(){
-    return CLUBS;
-  }
-
   static get SUITES(){
     return SUITES;
   }
