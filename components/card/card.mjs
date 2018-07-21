f3431e2dd0b5b7aa5c3813073f8559edf94ac893
Scott Davis <scott@thirstyhead.com>
2018-07-21 14:45:22 -0600

    integrated card.mjs with ttyw-card

--- components/card/card.mjs
+++ components/card/card.mjs
@@ -8,19 +8,19 @@ const SUITES = Object.freeze({
 });
 
 const RANKS = Object.freeze({
-  TWO   : {name:'deuce', rank:1},
-  THREE : {name:'three', rank:2},
-  FOUR  : {name:'four', rank:3},
-  FIVE  : {name:'five', rank:4},
-  SIX   : {name:'six', rank:5},
-  SEVEN : {name:'seven', rank:6},
-  EIGHT : {name:'eight', rank:7},
-  NINE  : {name:'nine', rank:8},
-  TEN   : {name:'ten', rank:9},
-  JACK  : {name:'jack', rank:10},
-  QUEEN : {name:'queen', rank:11},
-  KING  : {name:'king', rank:12},
-  ACE   : {name:'ace', rank:13}
+  TWO   : {name:'deuce', rank:1, display:'2'},
+  THREE : {name:'three', rank:2, display:'3'},
+  FOUR  : {name:'four', rank:3, display:'4'},
+  FIVE  : {name:'five', rank:4, display:'5'},
+  SIX   : {name:'six', rank:5, display:'6'},
+  SEVEN : {name:'seven', rank:6, display:'7'},
+  EIGHT : {name:'eight', rank:7, display:'8'},
+  NINE  : {name:'nine', rank:8, display:'9'},
+  TEN   : {name:'ten', rank:9, display:'10'},
+  JACK  : {name:'jack', rank:10, display:'J'},
+  QUEEN : {name:'queen', rank:11, display:'Q'},
+  KING  : {name:'king', rank:12, display:'K'},
+  ACE   : {name:'ace', rank:13, display:'A'}
 });
 
 export class Card{
@@ -50,6 +50,14 @@ export class Card{
 
   toString(){
     return `${RANKS[this.rank].name} of ${SUITES[this.suite].name}`
+  }
+
+  get displaySuite(){
+    return SUITES[this.suite].name;
+  }
+
+  get displayRank(){
+    return RANKS[this.rank].display;
   }
 
   static get SUITES(){
--- components/card/index.mjs
+++ components/card/index.mjs
@@ -1,147 +1,151 @@
 'use strict';
 
-window.customElements.define('ttyw-card',
-  class TtywCard extends HTMLElement{
-    constructor(){
-      super();
-    }
+import {Card} from '/components/card/card.mjs';
 
-    connectedCallback(){
-      let shadowRoot = this.attachShadow({mode:'open'});
-      shadowRoot.appendChild(this.styleBlock);
-      shadowRoot.appendChild(this.bodyBlock);
-    }
+class TtywCard extends HTMLElement{
+  constructor(){
+    super();
+    this.card = new Card(this.suite, this.rank);
+  }
 
-    get styleBlock(){
-      let styleBlock = document.createElement('style');
-      styleBlock.innerHTML = `
-        .card{
-          border: 1px solid black;
-          padding: 0.5em;
-          width: 5em;
-          background-color: white;
-          margin: 0 auto;
-        }
+  connectedCallback(){
+    let shadowRoot = this.attachShadow({mode:'open'});
+    shadowRoot.appendChild(this.styleBlock);
+    shadowRoot.appendChild(this.bodyBlock);
+  }
 
-        .card > header{
-          font-size: 1em;
-          width: 1em;
-        }
+  get styleBlock(){
+    let styleBlock = document.createElement('style');
+    styleBlock.innerHTML = `
+      .card{
+        border: 1px solid black;
+        padding: 0.5em;
+        width: 5em;
+        background-color: white;
+        margin: 0 auto;
+      }
 
-        .card > .card-center{
-          font-size: 3em;
-        }
+      .card > header{
+        font-size: 1em;
+        width: 1em;
+      }
 
-        .card > footer{
-          font-size: 1em;
-          width: 1em;
-          padding-right: 4em;
-          transform: scale(-1, -1);
-        }
+      .card > .card-center{
+        font-size: 3em;
+      }
 
-        /*********** HEARTS ***********/
-        .hearts{
-          color: red;
-        }
+      .card > footer{
+        font-size: 1em;
+        width: 1em;
+        padding-right: 4em;
+        transform: scale(-1, -1);
+      }
 
-        .hearts > header::after{
-          /* content: '&hearts;'; */
-          content: '\\A\\2665';
-        }
+      /*********** HEARTS ***********/
+      .hearts{
+        color: red;
+      }
 
-        .hearts > .card-center::after{
-          content: '\\2665';
-        }
+      .hearts > header::after{
+        /* content: '&hearts;'; */
+        content: '\\A\\2665';
+      }
 
-        .hearts > footer::after{
-          content: '\\A\\2665';
-        }
+      .hearts > .card-center::after{
+        content: '\\2665';
+      }
 
-        /*********** SPADES ***********/
-        .spades{
-          color: black;
-        }
+      .hearts > footer::after{
+        content: '\\A\\2665';
+      }
 
-        .spades > header::after{
-          /* content: '&spades;'; */
-          content: '\\A\\2660';
-        }
+      /*********** SPADES ***********/
+      .spades{
+        color: black;
+      }
 
-        .spades > .card-center::after{
-          content: '\\2660';
-        }
+      .spades > header::after{
+        /* content: '&spades;'; */
+        content: '\\A\\2660';
+      }
 
-        .spades > footer::after{
-          content: '\\A\\2660';
-        }
+      .spades > .card-center::after{
+        content: '\\2660';
+      }
 
-        /*********** CLUBS ***********/
-        .clubs{
-          color: black;
-        }
+      .spades > footer::after{
+        content: '\\A\\2660';
+      }
 
-        .clubs > header::after{
-          /* content: '&clubs;'; */
-          content: '\\A\\2663';
-        }
+      /*********** CLUBS ***********/
+      .clubs{
+        color: black;
+      }
 
-        .clubs > .card-center::after{
-          content: '\\2663';
-        }
+      .clubs > header::after{
+        /* content: '&clubs;'; */
+        content: '\\A\\2663';
+      }
 
-        .clubs > footer::after{
-          content: '\\A\\2663';
-        }
+      .clubs > .card-center::after{
+        content: '\\2663';
+      }
 
-        /*********** DIAMONDS ***********/
-        .diamonds{
-          color: red;
-        }
+      .clubs > footer::after{
+        content: '\\A\\2663';
+      }
 
-        .diamonds > header::after{
-          /* content: '&diamonds;'; */
-          content: '\\A\\2666';
-        }
+      /*********** DIAMONDS ***********/
+      .diamonds{
+        color: red;
+      }
 
-        .diamonds > .card-center::after{
-          content: '\\2666';
-        }
+      .diamonds > header::after{
+        /* content: '&diamonds;'; */
+        content: '\\A\\2666';
+      }
 
-        .diamonds > footer::after{
-          content: '\\A\\2666';
-        }
-      `;
+      .diamonds > .card-center::after{
+        content: '\\2666';
+      }
 
-      return styleBlock;
-    }
+      .diamonds > footer::after{
+        content: '\\A\\2666';
+      }
+    `;
 
-    get bodyBlock(){
-      let bodyBlock = document.createElement('div');
-      bodyBlock.classList.add('card');
-      bodyBlock.classList.add(this.suite.toLowerCase());
-      bodyBlock.innerHTML = `
-        <header>${this.rank}</header>
-        <div class="card-center"></div>
-        <footer>${this.rank}</footer>
-      `;
+    return styleBlock;
+  }
 
-      return bodyBlock;
-    }
+  get bodyBlock(){
+    let bodyBlock = document.createElement('div');
+    bodyBlock.classList.add('card');
+    bodyBlock.classList.add(this.card.displaySuite);
+    bodyBlock.innerHTML = `
+      <header>${this.card.displayRank}</header>
+      <div class="card-center"></div>
+      <footer>${this.card.displayRank}</footer>
+    `;
 
-    get suite(){
-      return this.getAttribute('suite');
-    }
+    return bodyBlock;
+  }
 
-    set suite(value){
-      this.setAttribute('suite', value);
-    }
+  get suite(){
+    return this.getAttribute('suite');
+  }
 
-    get rank(){
-      return this.getAttribute('rank');
-    }
+  set suite(value){
+    this.setAttribute('suite', value);
+  }
 
-    set rank(value){
-      this.setAttribute('rank', value);
-    }
+  get rank(){
+    return this.getAttribute('rank');
+  }
 
-  });
+  set rank(value){
+    this.setAttribute('rank', value);
+  }
+
+}
+
+window.customElements.define('ttyw-card', TtywCard);
