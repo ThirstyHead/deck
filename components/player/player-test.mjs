1ae3859925c4fc2d04a75c074eb01ca7841b55bd
Scott Davis <scott@thirstyhead.com>
2018-07-23 12:27:14 -0600

    cleaned up player tests

--- components/player/player-test.mjs
+++ components/player/player-test.mjs
@@ -21,23 +21,26 @@ tests.set("player can draw() and play() cards", () => {
   p.hand.draw('card1');
   p.hand.draw('card2');
   const cardCountAfterDrawing = p.hand.cardCount;
-  p.hand.play();
+  const playedCard = p.hand.play();
   const cardCountAfterPlaying = p.hand.cardCount;
 
   return 0 === initialCount &&
          2 === cardCountAfterDrawing &&
-         1 === cardCountAfterPlaying;
+         1 === cardCountAfterPlaying &&
+         playedCard === 'card1';
 });
 
 tests.set("new players have separate private decks", () => {
   const p1 = new Player('Suzi');
   const p2 = new Player('John');
 
-  const initialCount = p1.hand.cardCount;
   p1.hand.draw('cardXXX');
-  const cardCountAfterDrawing = p1.hand.cardCount;
+  p2.hand.draw('cardYYY');
+  p2.hand.draw('cardZZZ');
 
-  return 0 === initialCount &&
-         1 === cardCountAfterDrawing &&
-         0 === p2.hand.cardCount;
+  return p1.hand.cardCount === 1 &&
+         p1.hand.play() === 'cardXXX' &&
+         p2.hand.cardCount === 2 &&
+         p2.hand.play() === 'cardYYY' &&
+         p2.hand.play() === 'cardZZZ';
 });
